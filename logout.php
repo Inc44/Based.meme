4dd1fa1fc63db8cf26cd9dd42052b771a410e3eb
Inc <121856799+Inc44@users.noreply.github.com>
2025-04-19 11:42:57 +0200

    logout

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$_SESSION = [];
+session_destroy();
+if (isset($_COOKIE["remember_token"])) {
+	require_once "db_connect.php";
+	try {
+		$pdo = getDbConnection();
+		$stmt = $pdo->prepare("DELETE FROM user_sessions WHERE session_id = ?");
+		$stmt->execute([$_COOKIE["remember_token"]]);
+	} catch (PDOException $e) {
+		throw $e;
+	} catch (Exception $e) {
+		throw $e;
+	}
+	setcookie("remember_token", "", time() - 3600, "/");
+}
+header("Location: login.html");
+exit();
+?>
